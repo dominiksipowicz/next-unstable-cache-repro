3322e5d5f237aa4bbda5808bdc936b0afd0824cd
Luis Alvarez <luis@vercel.com>
2023-11-14 08:38:02 -0500

    Changes

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -1,20 +1,20 @@
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
+import { unstable_cache } from 'next/cache';
+import { cache } from 'react';
 
 const getData = async (page: string) => {
   console.log(`
    === api call with page: ${page} ===
    `);
   const res = await fetch(`http://localhost:3000/${page}`, {
-    cache: "no-store",
+    cache: 'no-store',
   });
 
-  return res;
+  return res.json();
 };
 
 const getDataReactCache = cache(getData);
 
-const getDataUnstableCache = unstable_cache(getData, ["my-tag"]);
+const getDataUnstableCache = unstable_cache(getData, ['my-tag']);
 
 const api = {
   call: (page: string) => getDataUnstableCache(page),
